81d1afabb3597f1987ee0cac4e87ef654073bc8e
mrwangyou <mrwangyou@qq.com>
2022-08-29 11:52:34 +0800

    怎么没有高亮

---
+++ gymTest.py
@@ -0,0 +1,39 @@
+import numpy as np 
+import cv2 
+import matplotlib.pyplot as plt
+import PIL.Image as Image
+import gym
+import random
+
+from gym import Env, spaces
+import time
+
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL 
+
+x = np.abs(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
